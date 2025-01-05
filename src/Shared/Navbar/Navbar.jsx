3165f859fb36cc287be0c6a816dc1e712fbebfc1
MD HABIBUL BASAR SHOHAG <mdhabibulbasarshohag1@gmail.com>
2025-01-05 11:05:25 +0600

    comm

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
-        <div>
+        <nav>
+            <div>
+                <Link to="/"></Link>
+            </div>
 
-        </div>
+        </nav>
     );
 };
 
